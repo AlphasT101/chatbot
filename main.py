0eba8eeb8cbb4da2b8c9aa7064908febcf61478b
AlphasT101 <anlexalphast@gmail.com>
2023-10-03 13:24:28 +0600

    files updated

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
+# if you get this error : AttributeError: module 'time' has no attribute 'clock'
+# then replace the required lines with time.perf_counter()
+
 import os
 from aiml import Kernel
+import sys
 
-folder_select = int(input("Enter 1 to select laptop folder or enter 2 to select PC folder: "))
 laptop_folder = "C:/Users/User/Documents/coding files/chatbot/database"
-pc_folder = "E:/AlphasT101/coding files/chatbot/database"
+pc_folder_windows = "D:/coding files/chatbot/database"
+pc_folder_ubuntu = "/media/raghib/disk/coding files/chatbot/database"
 aiml_folder = ""
 
-if folder_select == 1:
+folder_select = input("Enter 1 to select laptop folder or enter 2 to select PC folder(windows) or enter 3 to select PC folder(Ubuntu): ")
+
+if folder_select == "1" :
     aiml_folder = laptop_folder
-elif folder_select == 2:
-    aiml_folder = pc_folder
+elif folder_select == "2" :
+    aiml_folder = pc_folder_windows
+elif folder_select == "3" :
+    aiml_folder = pc_folder_ubuntu
+else:
+    print("FOLDER PATH ERROR")
+    sys.exit()
 
 class SimpleChatbot:
     def __init__(self):
@@ -27,7 +38,6 @@ class SimpleChatbot:
 
 if __name__ == "__main__":
     chatbot = SimpleChatbot()
-
     print("Chatbot: Hi there! How can I help you? Type 'exit' to end the conversation.")
 
     while True:
